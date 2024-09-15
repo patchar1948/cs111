94f6e4073ab0b75c1ecd872005ebeb9e8f755897
patchar1948 <patcha.mekchang@gmail.com>
2024-09-16 02:42:47 +0700

    mee

--- 204111/Extra/meeneoi.py
+++ 204111/Extra/meeneoi.py
@@ -1,36 +1,52 @@
-# def main():
-#     test_meeny()
+def main():
+    test_meeny()
 def eeny_meeny(name_list: list[str], rhyme_len: int=4) -> str:
     # หาตำแหน่องของอักษรตัวแรกของแต่ละคำ
     a = [0] + list(map(lambda x: len(x), name_list))
+    b = list(map(lambda x: len(x), name_list))
     list_index = []
     for i in range(len(a) - 2):
         list_index += [a[i + 1] + a[i]]
     list_index = [0] + list_index
+    # สร้าง dict เก็บตัวแปล
+    dict_name = dict()
+    for i in range(len(list_index)):
+        dict_name[list_index[i]] = name_list[i]
+    # return (dict_name)
 
     # รวมชื่อเป็น list เดียว
     name_list_sum = ''.join(name_list)
 
-    # ทำจนกว่าจะได้ตำแน่งที่เหลือตัวเดียว
+    # # ทำจนกว่าจะได้ตำแน่งที่เหลือตัวเดียว
     i = 0
     count = 0
     while len(list_index) > 1:
-        i += 1
-        if name_list_sum[i] == '*':
-            i += 1
-        if count == 4:
-            name_list_sum.replace(name_list_sum[i], '*')
+        if i >= len(name_list_sum):
+            i = 0
+        if name_list_sum[i] != '*':
+            count += 1
+
+        if count == rhyme_len:
+            if i in list_index: # ตำแหน่งนั้นตรงกับ list_index
+                # "" + "*"* + ""
+                name_list_sum = name_list_sum[:i] + ("*" * len(dict_name[i])) + (name_list_sum[i + len(dict_name[i]):])
+                list_index.remove(i)
+            else: 
+                name_list_sum = name_list_sum[:i] + '*' + name_list_sum[i + 1:]
+            # =========debug=========
+            # print("=========", i, "=========")
+            # print(dict_name)
+            # print(name_list_sum)
+            # print(i)
+            # print(list_index)
             # reset count
             count = 0
-            if i in list_index: # ตำแหน่งนั้นตรงกับ list_index
-                list_index.remove(i)
-        else:
+
+        i += 1
+        
+    # print(dict_name[list_index[0]])
+    return dict_name[list_index[0]]
             
-
-        
-        
-
-
 
 def test_meeny():
     assert eeny_meeny(['John', 'Ann', 'Tom']) == 'John'
@@ -41,5 +57,5 @@ def test_meeny():
     print("All tests passed!")
 
 if __name__ == '__main__':
-    print(eeny_meeny(['John', 'Ann', 'Tom']))
-    # main()
+    # print(eeny_meeny(['John', 'Ann', 'Tom']))
+    main()
